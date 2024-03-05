37fa4b1b189041c77983b9e95a80bb67982e52dc
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-03-05 11:07:12 +0800

    Add: Icons to navigation bar

--- app/(components)/nav.jsx
+++ app/(components)/nav.jsx
@@ -1,7 +1,21 @@
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Link from "next/link"
+
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <div>
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/TicketPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div className="text-default-text">johnandersyap510@gmail.com</div>
+    </nav>
+  );
 }
 
 export default Nav
