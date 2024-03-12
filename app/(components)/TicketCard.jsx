259d80b1072d94472e31792c866d5363811d9c67
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-03-12 15:42:10 +0800

    Add: Ticket default styling

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -5,13 +5,20 @@ import StatusDisplay from "./StatusDisplay"
 
 const TicketCard = () => {
   return (
-    <div>
-        <DeleteBlock />
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
         <PriorityDisplay />
-        <ProgressDisplay />
-        <StatusDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <h4>Ticket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2"/>
+      <p className="whitespace-pre-wrap">Ticket description goes here. Please do this ticket</p>
+      <ProgressDisplay />
+      <StatusDisplay />
     </div>
-  )
+  );
 }
 
 export default TicketCard
