1f1be6703d0ca95bbfca88f3b99a27fa2828b2d1
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-03-13 14:54:03 +0800

    Fix: faX typo

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,4 +1,4 @@
-import { fax } from "@fortawesome/free-solid-svg-icons";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const DeleteBlock = () => {
