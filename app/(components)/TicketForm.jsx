bb1ca860e3f0d936b6ada930ea751a6db96125fb
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-03-19 09:34:02 +0800

    Add: Edit Ticket Title and stylings

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -4,25 +4,43 @@ import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const TicketForm = () => {
-    const startingTicketData = {
-        title: "",
-        description: "",
-        priority: 1,
-        progress: 0,
-        status: "not started",
-        category: "Hardware Problem",
-    }
 
-    const [formData, setFormData] = useState(startingTicketData)
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+  }
+
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "not started",
+    category: "Hardware Problem",
+  };
+
+  const [formData, setFormData] = useState(startingTicketData);
   return (
-    <div>
-        <form className="flex justify-center" >
-            <h3>Create your ticket</h3>
-            <label>Title</label>
-            <input type="text" id="title" name="title" onChange={handleChange} required={true} value={formData.title}/>
-        </form>
+    <div className="flex justify-center">
+      <form>
+        <h3>Create your ticket</h3>
+        <label>Title</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default TicketForm
+export default TicketForm;
