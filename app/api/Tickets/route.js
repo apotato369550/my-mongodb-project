4d1a01f11c9f5ec43a62c8553fbe826718a53b33
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-03-23 15:26:24 +0800

    Fixed bug by editing .env.local: Not uploading to database

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,15 +1,16 @@
-import Ticket from "../../(models)/Ticket";
+import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server";
-
+// 1:31:57
 export async function POST(req) {
     console.log("POST RAN");
     try {
-        const body = await req.json()
-        const ticketData = body.formData
-        await Ticket.create(ticketData)
+        console.log("try RAN");
+        const body = await req.json();
+        const ticketData = body.formData;
+        await Ticket.create(ticketData);
         return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
     } catch (error) {
-        console.log(error)
-        return NextResponse.json({message: "Error", error}, { status: 500})
+        console.log(error);
+        return NextResponse.json({message: "Error", error}, { status: 500});
     }
 }
