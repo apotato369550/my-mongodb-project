3bdc8deb5fe86dd71a53e1bc0553d89af6a89970
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-03-24 14:34:00 +0800

    Add: GET async function

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,6 +1,6 @@
 import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server";
-// 1:31:57
+
 export async function POST(req) {
     console.log("POST RAN");
     try {
@@ -9,6 +9,19 @@ export async function POST(req) {
         const ticketData = body.formData;
         await Ticket.create(ticketData);
         return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({message: "Error", error}, { status: 500});
+    }
+}
+
+export async function GET() {
+    try {
+        const tickets = await Ticket.find();
+        return NextResponse.json(
+          { tickets },
+          { status: 201 }
+        );
     } catch (error) {
         console.log(error);
         return NextResponse.json({message: "Error", error}, { status: 500});
