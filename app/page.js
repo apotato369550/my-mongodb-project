89aa29488dc8a7b4b66c12f40da9c4b74e8d534f
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-03-25 09:23:10 +0800

    Add: getTickets async function

--- app/page.js
+++ app/page.js
@@ -1,6 +1,21 @@
 import TicketCard from "./(components)/TicketCard"
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    const res = await fetch("https://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    })
+
+    return res.json();
+  } catch (error) {
+    console.log("Failed to get tickets", error)
+  }
+}
+
+const Dashboard = async () => {
+
+  const { tickets } = await getTickets();
+
   return (
     <div className="p-5">
       <div className="lg:grid grid-cols-2 xl:grid-cols-4">
