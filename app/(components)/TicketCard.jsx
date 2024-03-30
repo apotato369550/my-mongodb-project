5a80757df91e222e0ae2b552a25beafeafbc50e6
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-03-30 15:06:52 +0800

    Add: Dynamic status and progress display

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -21,10 +21,10 @@ const TicketCard = ({ticket}) => {
       <div className="flex mt-2">
         <div className="flex flex-col">
           <p className="text-xs my-1">{ticket.createdAt}</p>
-          <ProgressDisplay />
+          <ProgressDisplay progress={ticket.progress} />
         </div>
         <div className="ml-auto flex items-end">
-          <StatusDisplay />
+          <StatusDisplay status={ticket.status}/>
         </div>
       </div>
     </div>
