d6f513827badfca8cb5f4b341707ed00ed93cbc5
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-04-04 08:27:06 +0800

    Edit: add link to titlecard

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -23,26 +23,26 @@ const TicketCard = ({ticket}) => {
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
-        <PriorityDisplay priority={ticket.priority}/>
+        <PriorityDisplay priority={ticket.priority} />
         <div className="ml-auto">
-          <DeleteBlock id={ticket._id}/>
+          <DeleteBlock id={ticket._id} />
         </div>
       </div>
-      <h4>{ticket.title}</h4>
-      <hr className="h-px border-0 bg-page mb-2" />
-      <p className="whitespace-pre-wrap">
-        {ticket.description}
-      </p>
-      <div className="flex-grow"></div>
-      <div className="flex mt-2">
-        <div className="flex flex-col">
-          <p className="text-xs my-1">{formatTimeStamp(ticket.createdAt)}</p>
-          <ProgressDisplay progress={ticket.progress} />
+      <Link>
+        <h4>{ticket.title}</h4>
+        <hr className="h-px border-0 bg-page mb-2" />
+        <p className="whitespace-pre-wrap">{ticket.description}</p>
+        <div className="flex-grow"></div>
+        <div className="flex mt-2">
+          <div className="flex flex-col">
+            <p className="text-xs my-1">{formatTimeStamp(ticket.createdAt)}</p>
+            <ProgressDisplay progress={ticket.progress} />
+          </div>
+          <div className="ml-auto flex items-end">
+            <StatusDisplay status={ticket.status} />
+          </div>
         </div>
-        <div className="ml-auto flex items-end">
-          <StatusDisplay status={ticket.status}/>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 }
