3ee46506e1a7c4130b0b09b1a59f38350afbc92c
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-04-12 13:30:18 +0800

    Add: Finalized ticket update functionality

--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -22,4 +22,20 @@ export async function DELETE(req, {params}) {
     }
 }
 
-// to make update function here
+export async function PUT(req, { params }) {
+    console.log("put ran")
+    try {
+        const { id } = params;
+        const body = await req.json();
+        const ticketData = body.formData;
+
+        const updateTicketData = await Ticket.findByIdAndUpdate(id, {
+            ...ticketData,
+        })
+
+        return NextResponse.json({ message: "Ticket Updated" }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: "Error", error }, { status: 500 });
+    }
+}
+
